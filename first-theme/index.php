8834ac18bab7f1df232bc92219b40091154cb02c
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2024-07-05 17:50:35 -0700

    Add files via upload

---
+++ first-theme/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ;?> >
+<head>
+<meta charset="<?php bloginfo('charset');?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title><?php bloginfo('name');?></title>
+<link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet" type="text/css">
+</head>
+<body <?php body_class(); ?>>
+</body>
+</html>
