a61736946027b7c317b9d3b1e6e54ba2958485e9
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2024-07-10 21:45:56 -0700

    Add files via upload

--- single.php
+++ single.php
@@ -12,30 +12,16 @@ if we have any posts or pages, show them !!!-->
 <?php while(have_posts()): the_post() ; ?> 
 <article class="post">
 <h2 class="title">
-<a href="<?php the_permalink() ;?>">
+
 <?php the_title() ; ?>
-</a>
+
 </h2>
 <div class="meta">
 <span><b>Posted By:</b> <?php the_author();?></span>
 <span><b>Posted On:</b> <?php the_time('F j, Y g:i a')  ;?></span>
 </div>
 <!-- close meta -->
-
-<div class="thumbnail">
-<?php  if(has_post_thumbnail()) : ?>
-<a href="<?php the_permalink() ;?>">
-<?php the_post_thumbnail(); ?>
-</a>
-<?php endif ?>
-</div>
-<!-- end thumbnail -->
-
-<?php the_excerpt() ; ?>
-<span class="block">
-<a href="<?php the_permalink() ;?>"> Read More about <?php the_title(); ?>  </a>
-</span>
-
+<?php the_content() ; ?>
 
 
 </article>
@@ -45,6 +31,7 @@ if we have any posts or pages, show them !!!-->
 <?php echo wpautop('Sorry, no posts were found!'); ?>
 </h2>
 <?php endif; ?>
+<?php comments_template();?>
 </main>
 
 
